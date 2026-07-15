1525a7d7cdd4957c01c1a7131a0b70c77137f3cc
ms7445476-commits <ms7445476@gmail.com>
2026-07-14 23:09:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Punnet-Square-Simulation/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
